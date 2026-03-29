b77e45ac889a055711a4dceb98f4e41bc32ab249
Kirus1930 <k_eryomin00@mail.ru>
2026-03-29 12:25:18 +0300

    dotnet build

--- ModularApp/obj/Debug/net10.0/ModularApp.AssemblyInfo.cs
+++ ModularApp/obj/Debug/net10.0/ModularApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModularApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f25175a4531effd2481596ccf762296d707de7a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8efb7acb5a8726bba4f49bba84180d1fa39d80b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModularApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModularApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
