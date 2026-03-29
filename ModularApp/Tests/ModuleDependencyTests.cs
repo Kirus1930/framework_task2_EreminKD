8bb867f8f4d14104c9291f02c03fb33963c147ee
Kirus1930 <k_eryomin00@mail.ru>
2026-03-29 11:39:52 +0300

    Create ModuleDependencyTests.cs

---
+++ ModularApp/Tests/ModuleDependencyTests.cs
@@ -0,0 +1,57 @@
+using Xunit;
+using ModularApp.Core;
+
+namespace ModularApp.Tests
+{
+    public class ModuleDependencyTests
+    {
+        [Fact]
+        public void Should_Resolve_Order_Correctly()
+        {
+            var modules = new List<IModule>
+            {
+                new ModuleA(),
+                new ModuleB(),
+                new ModuleC()
+            };
+
+            var resolver = new ModuleDependencyResolver();
+            var result = resolver.Resolve(modules);
+
+            var names = result.Select(m => m.Name).ToList();
+
+            Assert.Equal(new List<string> { "C", "B", "A" }, names);
+        }
+
+        [Fact]
+        public void Should_Throw_When_Module_Missing()
+        {
+            var modules = new List<IModule>
+            {
+                new ModuleA() // требует B
+            };
+
+            var resolver = new ModuleDependencyResolver();
+
+            var ex = Assert.Throws<ModuleException>(() => resolver.Resolve(modules));
+
+            Assert.Contains("Missing module", ex.Message);
+        }
+
+        [Fact]
+        public void Should_Throw_On_Cycle()
+        {
+            var modules = new List<IModule>
+            {
+                new CycleA(),
+                new CycleB()
+            };
+
+            var resolver = new ModuleDependencyResolver();
+
+            var ex = Assert.Throws<ModuleException>(() => resolver.Resolve(modules));
+
+            Assert.Contains("Cycle", ex.Message);
+        }
+    }
+}
