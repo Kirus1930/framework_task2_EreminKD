4b74f19f03fe42ff37bb266c171205478ee44062
Kirus1930 <k_eryomin00@mail.ru>
2026-03-31 10:10:01 +0300

    fix problem with adding new module

--- ModularApp/obj/Debug/net10.0/ModularApp.AssemblyInfo.cs
+++ ModularApp/obj/Debug/net10.0/ModularApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModularApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08119a07580cdf874bfafa59006e82c1f5191b4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccdf3d48f805438efad813c9402488993bfc8d1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModularApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModularApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
